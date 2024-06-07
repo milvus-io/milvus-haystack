3b6df3a14207ddfc38d735f8c706bf2290877e1b
Amir Karbasi <amir@karbasi.dev>
2024-06-07 14:16:22 -0400

    Add test cases
    Signed-off-by: Amir Karbasi <amir@karbasi.dev>

--- tests/test_embedding_retriever.py
+++ tests/test_embedding_retriever.py
@@ -44,3 +44,81 @@ class TestMilvusEmbeddingTests:
         query_embedding = [-10.0] * 128
         res = retriever.run(query_embedding)
         assert res["documents"] == documents
+
+    def test_to_dict(self, document_store: MilvusDocumentStore):
+        expected_dict = {
+            "type": "src.milvus_haystack.document_store.MilvusDocumentStore",
+            "init_parameters": {
+                "collection_name": "HaystackCollection",
+                "collection_description": "",
+                "collection_properties": None,
+                "connection_args": {"host": "localhost", "port": "19530", "user": "", "password": "", "secure": False},
+                "consistency_level": "Session",
+                "index_params": None,
+                "search_params": None,
+                "drop_old": True,
+                "primary_field": "id",
+                "text_field": "text",
+                "vector_field": "vector",
+                "partition_key_field": None,
+                "partition_names": None,
+                "replica_number": 1,
+                "timeout": None,
+            },
+        }
+        retriever = MilvusEmbeddingRetriever(document_store)
+        result = retriever.to_dict()
+
+        assert result["type"] == "src.milvus_haystack.milvus_embedding_retriever.MilvusEmbeddingRetriever"
+        assert result["init_parameters"]["document_store"] == expected_dict
+
+    def test_from_dict(self, document_store: MilvusDocumentStore):
+        retriever_dict = {
+            "type": "src.milvus_haystack.milvus_embedding_retriever.MilvusEmbeddingRetriever",
+            "init_parameters": {
+                "document_store": {
+                    "type": "milvus_haystack.document_store.MilvusDocumentStore",
+                    "init_parameters": {
+                        "collection_name": "HaystackCollection",
+                        "collection_description": "",
+                        "collection_properties": None,
+                        "connection_args": {
+                            "host": "localhost",
+                            "port": "19530",
+                            "user": "",
+                            "password": "",
+                            "secure": False,
+                        },
+                        "consistency_level": "Session",
+                        "index_params": None,
+                        "search_params": None,
+                        "drop_old": True,
+                        "primary_field": "id",
+                        "text_field": "text",
+                        "vector_field": "vector",
+                        "partition_key_field": None,
+                        "partition_names": None,
+                        "replica_number": 1,
+                        "timeout": None,
+                    },
+                },
+                "filters": None,
+                "top_k": 10,
+            },
+        }
+
+        retriever = MilvusEmbeddingRetriever(document_store)
+
+        reconstructed_retriever = MilvusEmbeddingRetriever.from_dict(retriever_dict)
+        for field in vars(reconstructed_retriever):
+            if field.startswith("__"):
+                continue
+            elif field == "document_store":
+                for doc_store_field in vars(document_store):
+                    if doc_store_field.startswith("__"):
+                        continue
+                    assert getattr(reconstructed_retriever.document_store, doc_store_field) == getattr(
+                        document_store, doc_store_field
+                    )
+            else:
+                assert getattr(reconstructed_retriever, field) == getattr(retriever, field)
